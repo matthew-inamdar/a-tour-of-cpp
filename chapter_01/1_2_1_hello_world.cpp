261e7451eca568b12a9d20c6153096f076b3862f
Matthew Inamdar <matt@inamdar.co.uk>
2024-02-18 12:50:42 +0000

    1.2 finished.

--- chapter_01/1_2_1_hello_world.cpp
+++ chapter_01/1_2_1_hello_world.cpp
@@ -1,1 +1,21 @@
-int main() {}
+#include <iostream>
+
+using namespace std;
+
+double square(double x);
+void print_square(double x);
+
+int main()
+{
+    print_square(1.234);
+}
+
+double square(double x)
+{
+    return x*x;
+}
+
+void print_square(double x)
+{
+    std::cout << "the square of " << x << " is " << square(x) << "\n";
+}
