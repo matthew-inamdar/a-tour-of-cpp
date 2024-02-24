a7aa9f6dd1cca853a72a57815d1ac0ec376ee831
Matthew Inamdar <matt@inamdar.co.uk>
2024-02-24 17:53:31 +0000

    Finished section 2.3.

---
+++ chapter_02/2_3_classes.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Vector {
+public:
+    Vector(int s) : elem{new double[s]}, sz{s} {}
+
+    ~Vector() {
+        delete[] elem;
+    }
+
+    double& operator[](int i) {
+        return elem[i];
+    }
+
+    int size() {
+        return sz;
+    }
+
+private:
+    double *elem;
+    int sz;
+};
+
+int main() {
+    Vector v(6);
+    std::cout << "Size: " << v.size() << std::endl;
+    std::cout << "[0]: " << v[0] << std::endl;
+
+    return 0;
+}
